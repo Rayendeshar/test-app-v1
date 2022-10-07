079b559b23cba1a0c87be5a4cc8b9fdc79a07c1a
Rayen <rayendaysaar@gmail.com>
2022-10-07 10:05:19 +0545

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App2 from './App2';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App2 />
+    <App />
 )
